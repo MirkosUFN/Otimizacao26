0ae668cb83084c21f27ef9bb3e421b1c012002b1
Gilberto <gmorales@outlook.com.br>
2026-02-23 08:56:18 -0300

    feat: Aula 23/02/26

--- exercicio1/lista.py
+++ exercicio1/lista.py
@@ -1,13 +1,13 @@
 import random
 
-numeros = [random.randint(1, 100) for _ in range(50)]
-media = sum(numeros) / len(numeros)
+lista = [random.randint(0, 100) for _ in range(50)]
+# Média
+m = sum(lista) / len(lista)
+# Menor distância
+md = min(abs(x - m) for x in lista)
+# Mais próximo
+mp = [x for x in lista if abs(x - m) == md]
 
-menor_diferenca = min(abs(num - media) for num in numeros)
-mais_proximos = [num for num in numeros if abs(num - media) == menor_diferenca]
-mais_proximos_unicos = list(set(mais_proximos))
-
-print(f"Lista de números:\n{numeros}")
-print(f"Média dos números:\n{media:.2f}")
-print(f"Menor diferença da média:\n{menor_diferenca:.2f}")
-print(f"Número(s) mais próximo(s) da média:\n{mais_proximos_unicos}")
+print(sorted(lista))
+print("\nMédia:", m)
+print("Números mais próximos da média:", mp)
