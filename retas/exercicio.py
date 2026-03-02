68f3f2006069f7d002bf247dd0eac268c32c910c
ThiagoMarzari <87370358+ThiagoMarzari@users.noreply.github.com>
2026-03-02 09:02:14 -0300

    Add files via upload

---
+++ retas/exercicio.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#y=ax+b -> codigo
+
+def funcao(a, b, x0, x1): 
+  result = []
+  for x in range(x0, x1):
+    result.append(a*x + b)
+  return result
+
+a = 5
+b = -11
+#Intervalo de x
+x0 = -2
+x1 = 5
+
+
+print(funcao(a, b, x0, x1))
+
+#Desenhar a reta
+plt.plot(funcao(a, b, x0, x1))
+plt.show()
