dfdfc413420e56a37d0db0c8aa6b284331ae12bf
Eduardo Ceretta <108603083+Duduceretta@users.noreply.github.com>
2026-03-16 09:09:18 -0300

    Refactor funcao to handle pivot and factor checks

--- diagonalize/eduardo.py
+++ diagonalize/eduardo.py
@@ -1,36 +1,42 @@
 def funcao(matriz):
     pivo = matriz[0][0]
-    for i in range(len(matriz[0])):
-        matriz[0][i] = matriz[0][i] / pivo
-    print(matriz)
+
+    if pivo != 1:
+        for i in range(len(matriz[0])):
+            matriz[0][i] = matriz[0][i] / pivo
+        print(matriz)
 
     fator = matriz[1][0]
-    for i in range(len(matriz[1])):
-        matriz[1][i] = matriz[1][i] - fator * matriz[0][i]
-    print(30*'===')
-    print(matriz)
+
+    if fator != 0:
+        for i in range(len(matriz[1])):
+            matriz[1][i] = ((- fator) * matriz[0][i]) + matriz[1][i] 
+        print(30*'===')
+        print(matriz)
 
     pivo = matriz[1][1]
-    for i in range(1, len(matriz[1])):
-        matriz[1][i] = matriz[1][i] / pivo
-    print(30*'===')
-    print(matriz)
+
+    if pivo != 1:
+        for i in range(1, len(matriz[1])):
+            matriz[1][i] = matriz[1][i] / pivo
+        print(30*'===')
+        print(matriz)
 
     fator = matriz[0][1]
-    for i in range(1, len(matriz[0])):
-        matriz[0][i] = matriz[0][i] - fator * matriz[1][i]
-    print(30*'===')
-    print(matriz)
+
+    if fator != 0:
+        for i in range(1, len(matriz[0])):
+            matriz[0][i] = ((- fator * matriz[1][i])) + matriz[0][i]
+        print(30*'===')
+        print(matriz)
 
     return [matriz[0][2], matriz[1][2]]
 
 
 matriz = [
-    [3, 2, 6],
-    [2, 3, 5]
+    [1, 1, 12],
+    [3, -1, 20]
 ]
 
 valores = funcao(matriz)
 print(valores)
-
-    
